601e1dd3f54c4c3c4c654468c8cee3107cb39d98
shreevyas <shreevyas0211@gmail.com>
2022-03-07 09:54:59 +0530

    TicTacToe Board created

---
+++ TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTokGame
+{
+    
+    public class TicTacToeGame
+    {
+        static char[] board = new char[9];
+        public static void BoardGame()
+        {
+            for(int i=1;i<=board.Length;i++)
+            {
+                Console.WriteLine(board[i] + "   ");
+            }
+        }
+    }
+}
